7550a6414a3db70f06dd8938fa156fefd25422b6
Taisei-Kawashima <132059916+Taisei-Kawashima@users.noreply.github.com>
2025-08-02 19:40:19 +0900

    RecipeRepositoryの作成、データ層がすべて完成

--- app/src/main/java/namake/recipebook/data/repository/RecipeRepository.kt
+++ app/src/main/java/namake/recipebook/data/repository/RecipeRepository.kt
@@ -1,2 +1,20 @@
 package namake.recipebook.data.repository
 
+import kotlinx.coroutines.flow.Flow
+import namake.recipebook.data.local.RecipeDao
+import namake.recipebook.data.model.Recipe
+
+// UI側（ViewModel）とデータベース（DAO）の仲介役
+class RecipeRepository(private val recipeDao: RecipeDao) {
+
+    // DAOにレシピの全件取得を依頼する
+    fun getAllRecipes(): Flow<List<Recipe>> {
+        return recipeDao.getAllRecipes()
+    }
+
+    // DAOにレシピの追加を依頼する
+    suspend fun insertRecipe(recipe: Recipe) {
+        recipeDao.insertRecipe(recipe)
+    }
+
+}
