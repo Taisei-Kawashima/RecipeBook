6f21374de0583ef750f89fda8811ea9b4a53becf
Taisei-Kawashima <132059916+Taisei-Kawashima@users.noreply.github.com>
2025-08-02 19:28:07 +0900

    data class Recipeの作成

--- app/src/main/java/namake/recipebook/data/model/Recipe.kt
+++ app/src/main/java/namake/recipebook/data/model/Recipe.kt
@@ -1,2 +1,12 @@
 package namake.recipebook.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val name: String = "",
+    val imagePath: String?
+)
